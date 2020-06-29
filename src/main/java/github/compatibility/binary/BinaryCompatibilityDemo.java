fb8d873b7177dbb4a9f92ff7345643fc3c95e0f6
elseifer <ddc.niqgniq@gmail.com>
2020-06-29 10:58:15 +0800

    add sample

---
+++ src/main/java/github/compatibility/binary/BinaryCompatibilityDemo.java
@@ -0,0 +1,32 @@
+package github.compatibility.binary;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author elseifer
+ * @version $Id: BinaryCompatibilityDemo.java, v 0.1 2020年06月28日 下午5:55 elseifer Exp $
+ */
+public class BinaryCompatibilityDemo {
+
+    public void demoKeySetView() {
+
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<String, String>();
+
+        long count = concurrentHashMap.mappingCount();
+
+        Set<String> strings = concurrentHashMap.keySet();
+
+    }
+
+    public void demoOption() {
+
+        Optional<String> optional = Optional.empty();
+
+    }
+
+    //-source 必须等于或低于 -target 的值必须一致，如 -source 1.8 -target 1.7 会报错 "javacTask: 源发行版 1.8 需要目标发行版 1.8"
+    //无论是显性、隐性地使用高版本的类、api，-source -target 不会报错；
+
+}
---
+++ src/main/java/github/compatibility/source/SourceCompatibilityDemo.java
@@ -0,0 +1,9 @@
+package github.compatibility.source;
+
+/**
+ * @author elseifer
+ * @version $Id: SourceCompatibilityDemo.java, v 0.1 2020年06月28日 下午4:19 elseifer Exp $
+ */
+public class SourceCompatibilityDemo {
+
+}
