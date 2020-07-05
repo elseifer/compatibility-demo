36df5c8fd98b705462827692049e479b4c41d1b8
elseifer <ddc.niqgniq@gmail.com>
2020-07-05 18:27:08 +0800

    add example

---
+++ src/main/java/github/compatibility/source/SampleClass.java
@@ -0,0 +1,23 @@
+
+package github.compatibility.source;
+
+import java.util.List;
+
+/**
+ * @author elseifer
+ * @version $Id: SampleClass.java, v 0.1 2020年07月05日 下午6:17 elseifer Exp $
+ */
+public class SampleClass {
+
+    //需要时再取消注释
+
+    //static class Baz<T> {
+    //    public static List<Baz<Object>> sampleMethod(Baz<Object> param) {
+    //        return null;
+    //    }
+    //}
+    //
+    //private static void bar(Baz arg) {
+    //    Baz element = Baz.sampleMethod(arg).get(0);
+    //}
+}
